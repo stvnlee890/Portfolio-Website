e1c6d44c47de6b5ed33fe63be2a4324918b8a21e
stvnlee890 <stvnlee890@gmail.com>
2022-06-24 15:11:57 -0700

    added sticky nav

--- script.js
+++ script.js
@@ -6,3 +6,8 @@ resume.addEventListener('click', function (){
   console.log('resume page')
 })
 
+const header = document.querySelector('.header')
+
+window.addEventListener('scroll', function(){
+  header.classList.toggle('sticky', window.scrollY > 0)
+})
