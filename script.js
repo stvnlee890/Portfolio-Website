58dc9d6210c77458eee50b4683bd29f89f996ebb
stvnlee890 <stvnlee890@gmail.com>
2022-06-24 17:23:28 -0700

    script.js possible starting point for removing nav when scrolled up

--- script.js
+++ script.js
@@ -6,8 +6,26 @@ resume.addEventListener('click', function (){
   console.log('resume page')
 })
 
-const header = document.querySelector('.header')
+// const header = document.querySelector('.header')
 
-window.addEventListener('scroll', function(){
-  header.classList.toggle('sticky', window.scrollY > 0)
+// window.addEventListener('scroll', function(){
+//   header.classList.toggle('sticky', window.scrollY > 0)
+// })
+
+const sticky = document.querySelector('.sticky')
+const topNavBar = document.querySelector('#top-nav-bar')
+let navBar = topNavBar.offsetTop;
+const navigation = document.querySelector('#navigation')
+
+
+// STRETCH GOAL
+// FIGURE SCROLL DOWN NAV DISAPPEARS, SCROLL UP NAV APPEARS
+window.addEventListener('scroll', function() {
+  let scroll = window.scrollY
+  let scrollDown = document.documentElement.scrollHeight
+  let lastScrollY = window.pageYOffset
+ console.log(lastScrollY)
 })
+let scrollDown = document.documentElement.scrollHeight
+console.log(scrollDown)
+console.log
