38e9c84123b21c9df8077fa25b6979ca909c12f1
stvnlee890 <stvnlee890@gmail.com>
2022-06-24 18:38:12 -0700

    experience content added

--- script.js
+++ script.js
@@ -20,12 +20,12 @@ const navigation = document.querySelector('#navigation')
 
 // STRETCH GOAL
 // FIGURE SCROLL DOWN NAV DISAPPEARS, SCROLL UP NAV APPEARS
-window.addEventListener('scroll', function() {
-  let scroll = window.scrollY
-  let scrollDown = document.documentElement.scrollHeight
-  let lastScrollY = window.pageYOffset
- console.log(lastScrollY)
-})
-let scrollDown = document.documentElement.scrollHeight
-console.log(scrollDown)
-console.log
+// window.addEventListener('scroll', function() {
+//   let scroll = window.scrollY
+//   let scrollDown = document.documentElement.scrollHeight
+//   let lastScrollY = window.pageYOffset
+//  console.log(lastScrollY)
+// })
+// let scrollDown = document.documentElement.scrollHeight
+// console.log(scrollDown)
+// console.log
