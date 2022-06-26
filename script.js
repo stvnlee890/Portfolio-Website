f5bf37ffb9572bb1ce93a759423e7128c1378ac9
stvnlee890 <stvnlee890@gmail.com>
2022-06-26 14:00:13 -0700

    added scrollIntoView functionality to page

--- script.js
+++ script.js
@@ -1,21 +1,52 @@
 
-const resume = document.querySelector('#resume')
-console.log(resume)
 
-resume.addEventListener('click', function (){
-  console.log('resume page')
+const navBar = document.querySelectorAll('.nav')
+console.log(navBar)
+
+const about = document.querySelector('#about')
+const projects = document.querySelector('#projects')
+const experience = document.querySelector('#experience')
+const contact = document.querySelector('#contact')
+
+const intro = document.querySelector('.introduction')
+const projectPage = document.querySelector('.project-page')
+const experiencePage = document.querySelector('.experience-page')
+const contactPage = document.querySelector('.contact-page')
+
+
+
+
+navBar.forEach(button => {
+  button.addEventListener('click', event => {
+    if(event.target === projects){
+    projectPage.scrollIntoView({
+      behavior : 'smooth'
+    })
+    }else if(event.target === experience){
+      experiencePage.scrollIntoView({
+        behavior : 'smooth'
+      })
+    }else if(event.target === about){
+      intro.scrollIntoView({
+        behavior : 'smooth'
+      })
+    }else if(event.target === contact){
+      contactPage.scrollIntoView({
+        behavior : 'smooth'
+      })
+    }
+  })
 })
 
-// const header = document.querySelector('.header')
 
-// window.addEventListener('scroll', function(){
-//   header.classList.toggle('sticky', window.scrollY > 0)
-// })
 
-const sticky = document.querySelector('.sticky')
-const topNavBar = document.querySelector('#top-nav-bar')
-let navBar = topNavBar.offsetTop;
-const navigation = document.querySelector('#navigation')
+
+window.addEventListener('click', event => {
+  console.log(event.target)
+})
+
+
+
 
 
 // STRETCH GOAL
@@ -29,3 +60,31 @@ const navigation = document.querySelector('#navigation')
 // let scrollDown = document.documentElement.scrollHeight
 // console.log(scrollDown)
 // console.log
+
+
+
+
+// BUTTON CAROUSEL
+
+// const carouselImage = document.querySelector('.carousel-images');
+// const carouselButtons = document.querySelectorAll ('.carousel-button');
+// const numberOfImages = document.querySelectorAll('carousel-images img').length;
+// let imageIndex = 1;
+// let translateX = 0;
+// console.log()
+// carouselButtons.forEach(button => {
+//   button.addEventListener('click', event => {
+//     if(event.target.id === 'previous') {
+//       if(imageIndex !== 1) {
+//         imageIndex--;
+//         translateX += 500;
+//       }
+//     }else{
+//       if(imageIndex !== numberOfImages) {
+//         imageIndex++;
+//         translateX-= 400;
+//       }
+//     }
+//     carouselImage.style.transform = `translateX(${translateX}px)`
+//   })
+// })
